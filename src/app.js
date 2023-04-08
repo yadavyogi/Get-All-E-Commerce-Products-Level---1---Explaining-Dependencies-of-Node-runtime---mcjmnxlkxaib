95cdaea48c21c319717c228e5b6bacf04a1ef870
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-04-08 11:34:19 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+// Importing products from products.json file
+const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+
+// Middlewares
+app.use(express.json());
+
+// Write GET endpoint for sending all the products to client here
+// Endpoint - /api/v1/products
+
+module.exports = app;
