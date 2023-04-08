5be0dc4f8d0e5c1f064fcab4f3587dd965dc6f2e
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2023-04-08 11:36:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
-
-// Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
-
-// Middlewares
-app.use(express.json());
-
-// Write GET endpoint for sending all the products to client here
-// Endpoint - /api/v1/products
-
+app.get("/api/v1/products", (req, res) => {
+  if (products.length === 0) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+  res.status(200).json({
+    status: "success",
+    message: "Product fetched successfully",
+    data: {
+      products
+    }
+  });
+});
 module.exports = app;
